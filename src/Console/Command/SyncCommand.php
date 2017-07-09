ccd3312f9629556d18c70d9d173db21aa29c143f
Ronan Bernard <ronan.bernard@imaginaires.org>
2017-07-09 12:52:18 +0200

    gitignore applied, basic parsing

--- src/Console/Command/SyncCommand.php
+++ src/Console/Command/SyncCommand.php
@@ -7,10 +7,12 @@ use Symfony\Component\Console\{
     Input\InputInterface,
     Output\OutputInterface
 };
-use StudioAPI\Model\Personnage;
 
 class SyncCommand extends Command
 {
+    private $modelFolder = 'src/StudioAPI/Model';
+    private $modelNamespace = '\\StudioAPI\\Model';
+
     protected function configure()
     {
         $this->setName('sync')
@@ -25,12 +27,45 @@ class SyncCommand extends Command
     }
 
     protected function detectModelFiles() {
-        // les classes ne sont pas encore loadées
-        // du coup il vaut mieux avoir une conf où lire l'emplacement,
-        // ou lire le domaine dont on va lire les \Model\*
-
+        // TODO gérer autrement le répertoire des model ?
+        foreach (new \DirectoryIterator($this->modelFolder) as $fileInfo) {
+            if($fileInfo->isDot()) continue;
+            $this->syncModelFile($fileInfo->getFilename());
+        }
     }
 
+    protected function syncModelFile(String $filename) {
+        $code = file_get_contents($this->modelFolder . '/' . $filename);
+        $classes = self::get_php_classes($code);
 
+        foreach ($classes as $class) {
+            $this->syncModel($code, $class);
+        }
+    }
+
+    protected function syncModel(&$fileCode, $className) {
+        echo "Sync class $className \n";
+        $reflection = new \ReflectionClass($this->modelNamespace . "\\" . $className);
+        $methods = $reflection->getMethods();
+        foreach ($methods as $method) {
+            echo "method $method \n found";
+        }
+    }
+
+    static function get_php_classes($php_code) {
+        $classes = array();
+        $tokens = token_get_all($php_code);
+        $count = count($tokens);
+        for ($i = 2; $i < $count; $i++) {
+            if (   $tokens[$i - 2][0] == T_CLASS
+                && $tokens[$i - 1][0] == T_WHITESPACE
+                && $tokens[$i][0] == T_STRING) {
+
+                $class_name = $tokens[$i][1];
+                $classes[] = $class_name;
+            }
+        }
+        return $classes;
+    }
 
 }
